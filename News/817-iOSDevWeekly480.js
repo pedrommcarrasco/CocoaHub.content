4b75eb0e0574f953f929ccce27b0978bbb9e3ec6
IRSHAD PC <irshadpc@yahoo.in>
2020-10-30 23:58:48 +0530

    Added | New | iOSDevWeekly 480

---
+++ News/817-iOSDevWeekly480.js
@@ -0,0 +1,12 @@
+{
+    "title" : "iOS Dev Weekly 480",
+    "description" : "Read Dave's curated selection of articles, news & tools from this week.",
+    "url" : "https://iosdevweekly.com/issues/480#start",
+    "curator" : {
+        "name" : "Irshad PC",
+        "url" : "https://twitter.com/_irshadpc"
+    },
+    "tags" : [
+        "newsletter"
+    ]
+}
